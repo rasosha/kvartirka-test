fbca3c4327ba87ec7b2e90f8819dddd878dca749
Andrei Rassokhin <rasosha@gmail.com>
2023-08-18 14:39:12 +0800

    fix: test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             priority
           />
         </section>
-        <>{children}</>
+        {children}
       </body>
     </html>
   );
