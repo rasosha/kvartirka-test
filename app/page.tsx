1b1ddbca4cf605851b6adfced21618b6bbb0c9c4
Andrei Rassokhin <rasosha@gmail.com>
2023-08-18 14:54:48 +0800

    fix: idiotFix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,5 @@ import List from "./components/List";
 
 export default async function Home() {
   const data = await getData();
-  console.log("mydata:>>", data);
-  return data ? <List data={data} /> : <div>1</div>;
+  return <List data={data} />;
 }
